e61aaebe1d3e5750794181a984662e08753235b7
Philip Dahlberg <philiparonkd@gmail.com>
2024-03-27 21:28:56 +0100

    Implemented all necessary functions Not quite like planned, loopless for now

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -30,3 +30,67 @@ Pseudocode:
         ...
             ...
 */
+const hands = ["ROCK", "PAPER", "SCISSORS"]
+let computerScore = 0;
+let playerScore = 0;
+let playerSelection = "";
+let roundResult = "";
+let computerSelection = "";
+
+//Randomly returns hand for computer
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);//random number from 0 to 2
+    computerSelection = hands[randomNumber];
+    return computerSelection;
+}
+//Gets player hand and converts to uppercase
+function getPlayerChoice() {
+    playerSelection = prompt("Write Rock, Paper or Scissors to make your choice: ");
+    playerSelection = playerSelection.toUpperCase();
+    return playerSelection;
+}
+//Checks winner of current hand selections
+function winCheck(playerSelection, computerSelection) {
+    roundResult = "";
+    if (playerSelection === computerSelection) {
+        roundResult = "Tie";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
+    playerSelection === "PAPER" && computerSelection === "ROCK" || 
+    playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        roundResult = "Win";
+    } else {
+        roundResult = "Loss";
+    }
+    return roundResult;
+}
+//Plays one round of RPS
+function playRound() {
+    getComputerChoice();
+    getPlayerChoice();
+    winCheck(playerSelection, computerSelection);
+    if (roundResult === "Win") {
+        playerScore++
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+    } else if (roundResult === "Loss") {
+        computerScore++
+        console.log(`You lose. ${computerSelection} beats ${playerSelection}`)
+    } else {
+        console.log(`Computer chose ${computerSelection}, it's a tie!`)
+    }
+}
+//Plays a B03 of RPS
+function playGame() {
+    //will implement a B03 loop here later, for now just a 5 round game
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    if (playerScore > computerScore) {
+        console.log(`You won the game! Your score: ${playerScore} | Computers score: ${computerScore}`);
+    } else {
+        console.log(`You lost the game. Your score: ${playerScore} | Computers score: ${computerScore}`)
+    }
+    playerScore = 0;
+    computerScore = 0;
+}
